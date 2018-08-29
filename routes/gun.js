952710f6f6bad0787241916680cb05306389ec8a
x <x@xs-MacBook-Pro.local>
2018-08-29 15:59:52 -0400

    pearls before swine

--- routes/gun.js
+++ routes/gun.js
@@ -1,25 +1,18 @@
 var Gun = require('gun')
-module.exports = function(app){
+module.exports = functi on(app){
 
     app.put(`/gunDB`, function(req, res) {
         console.log("hit gun");
         if (Gun.serve(req, res)){ return } // filters gun requests!
-        require('fs').createReadStream(require('path').join(__dirname, req.url)).on('error',function(){ // static files!
+        require('fs').createReadStream(require('path').join(__dirname, req.url)).on('error',function(){  
         
         res.send({"success": true});
         res.end();
         }).pipe(res); // stream
       
       
-    }),
-
-    app.put(`/uploadImageIPFS`, function(req, res) {
-        console.log("hit ipfs img");
-      console.log(req.body);
-      res.send('200');
-      
-      
     })
+
   
 
    
